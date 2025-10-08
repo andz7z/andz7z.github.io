bbe674589a4c8d0e2acecddba3f20028d42b3a3a
andz7z <al277@student.ugal.ro>
2025-10-08 04:58:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -193,7 +193,7 @@ setTimeout(() => {
 
     // Play notification sound
     const notifSound = new Audio("https://github.com/andz7z/andz7z.github.io/raw/main/notification.MP3");
-    notifSound.volume = 0.2;
+    notifSound.volume = 0.1;
     notifSound.play().catch(() => {});
 
     // Hide after 12 seconds
