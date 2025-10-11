e3c172d2addc1c8ad5c86bbf75da4f11d591dc8f
andz7z <al277@student.ugal.ro>
2025-10-12 00:20:30 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBGKXuWCXFn1JzKP6nNyKNhbBVO6rVfPzk",
+    authDomain: "andz-portfolio.firebaseapp.com",
+    projectId: "andz-portfolio",
+    storageBucket: "andz-portfolio.firebasestorage.app",
+    messagingSenderId: "808482872175",
+    appId: "1:808482872175:web:c526c7c2c69b40def4269a"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
