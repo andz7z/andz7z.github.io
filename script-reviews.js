798678cfdf0938d45e9d1b8c160e25578caf3cb5
andz7z <al277@student.ugal.ro>
2025-10-12 17:52:34 +0300

    Update script-reviews.js

--- script-reviews.js
+++ script-reviews.js
@@ -2,6 +2,7 @@
 const firebaseConfig = {
     apiKey: "AIzaSyCGn9V5dUPM3m_LxzQuiwDWB5vUc24bF6c",
     authDomain: "andz-reviews-67306.firebaseapp.com",
+    databaseURL: "https://andz-reviews-67306-default-rtdb.europe-west1.firebasedatabase.app",
     projectId: "andz-reviews-67306",
     storageBucket: "andz-reviews-67306.firebasestorage.app",
     messagingSenderId: "314000134063",
