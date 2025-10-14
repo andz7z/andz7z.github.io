9daf4b34d5b6e3f1354ed087eeb77c35d15a858b
andz7z <al277@student.ugal.ro>
2025-10-14 14:27:41 +0300

    Update script-reviews.js

--- script-reviews.js
+++ script-reviews.js
@@ -10,7 +10,8 @@ const firebaseConfig = {
 };
 firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
-
+let activeReviewId = null;
+let replyGender = 'male';
 // script-reviews.js
 // Presupunere: firebase este inițializat și ai db = firebase.database();
 // Include acest fișier DUPĂ firebase & firebase database script.
@@ -377,8 +378,6 @@ const modalTitle = document.getElementById('replies-title');
 const closeBtn = document.getElementById('replies-close');
 const sendBtn = document.getElementById('send-reply');
 const msgInputReply = document.getElementById('reply-message');
-let activeReviewId = null;
-let replyGender = 'male';
 // === TRIMITERE REPLY ===
 if (sendBtn) {
   sendBtn.onclick = () => {
