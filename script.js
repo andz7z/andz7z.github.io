bf4fa51422e77e2c1b185924d0cb8b69018e3784
andz7z <al277@student.ugal.ro>
2025-10-17 19:20:22 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-// Loader hide after 3 seconds
 window.addEventListener("load", () => {
   const loader = document.getElementById("loader");
-  setTimeout(() => {
-    loader.classList.add("hide");
-  }, 3000);
+  setTimeout(() => loader.classList.add("hide"), 3000);
+});
+
+const auraOverlay = document.querySelector(".aura-overlay");
+document.addEventListener("mousemove", (e) => {
+  const x = (e.clientX / window.innerWidth) * 100;
+  const y = (e.clientY / window.innerHeight) * 100;
+  auraOverlay.style.setProperty("--x", `${x}%`);
+  auraOverlay.style.setProperty("--y", `${y}%`);
 });
