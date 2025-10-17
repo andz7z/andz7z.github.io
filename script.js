8de55d3eb2413131dc02f84d34ee79d069655fa1
andz7z <al277@student.ugal.ro>
2025-10-17 15:26:24 +0300

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// === Active Glow on Click ===
-document.querySelectorAll('.glass').forEach(btn => {
-  btn.addEventListener('click', () => {
-    document.querySelectorAll('.glass').forEach(b => b.classList.remove('active'));
-    btn.classList.add('active');
-  });
-});
