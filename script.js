b4d5a798653601d76e0f941664f6310e5c64b96f
andz7z <al277@student.ugal.ro>
2025-10-19 02:17:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// Script doar pentru declanșarea ușoară a animației
 window.addEventListener("load", () => {
   const lines = document.querySelectorAll(".line");
   lines.forEach((line, index) => {
-    line.style.animationDelay = `${index * 0.8}s`;
+    line.style.animationDelay = `${3 + index * 0.8}s`;
   });
 });
