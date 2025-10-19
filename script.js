535c0a546a82153c9c6b2d287d75025029053419
andz7z <al277@student.ugal.ro>
2025-10-19 17:58:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,25 @@ setTimeout(() => {
     }, 100);
   }, 1000);
 }, 3000);
+setTimeout(() => {
+  const loader = document.querySelector('.loader-screen');
+  const landing = document.querySelector('.landing-page');
+
+  // blur out loading
+  loader.style.filter = "blur(15px)";
+  loader.style.opacity = "0";
+
+  setTimeout(() => {
+    loader.style.display = "none";
+    landing.classList.remove('hidden');
+
+    // blur in landing
+    setTimeout(() => {
+      landing.classList.add('active');
+      // după ce e activ, pornește efectul pe litere
+      document.querySelectorAll('.vision-title span').forEach((span, i) => {
+        span.style.animationPlayState = 'running';
+      });
+    }, 100);
+  }, 1000);
+}, 3000);
