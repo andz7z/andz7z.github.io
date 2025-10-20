3ad378cb3727a2d451d79cb7323770ede97db02b
andz7z <al277@student.ugal.ro>
2025-10-20 15:00:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 window.addEventListener("load", () => {
   const loader = document.querySelector(".loader-screen");
-  const videoBg = document.querySelector(".video-bg");
+  const video = document.getElementById("bg-video");
 
-  // așteaptă 3 secunde înainte de tranziție
+  // asigură-te că începe video-ul
+  video.play().catch(() => console.log("Autoplay blocat, dar e mutat."));
+
+  // după 3 secunde: fade out loader + fade in video
   setTimeout(() => {
     loader.classList.add("fade-out");
-    videoBg.classList.add("show");
+    video.classList.add("show");
   }, 3000);
 });
