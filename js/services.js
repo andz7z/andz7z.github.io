dd82bf576f2762cf7613841daf5a198389c5ba9c
andz7z <al277@student.ugal.ro>
2025-10-20 20:52:26 +0300

    Update services.js

--- js/services.js
+++ js/services.js
@@ -1,1 +1,17 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const next = document.querySelector("#services .next");
+  const prev = document.querySelector("#services .prev");
+
+  next.addEventListener("click", () => {
+    const items = document.querySelectorAll("#services .item");
+    document.querySelector("#services .slide").appendChild(items[0]);
+  });
+
+  prev.addEventListener("click", () => {
+    const items = document.querySelectorAll("#services .item");
+    document.querySelector("#services .slide").prepend(items[items.length - 1]);
+  });
+});
 
