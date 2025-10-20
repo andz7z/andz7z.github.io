2748f0a636a38e02d5e636dad8025a99c59e618e
andz7z <al277@student.ugal.ro>
2025-10-20 14:04:10 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,23 @@
+// Activează efectul overlap text
+document.querySelectorAll("[overlap-text]").forEach(e => {
+  e.innerHTML = [...e.innerText]
+    .map((c, i) => `<span style="--i:${i}">${c}</span>`)
+    .join("");
+});
+
 window.addEventListener("load", () => {
   const loader = document.querySelector(".loader-screen");
   const videoBg = document.querySelector(".video-background");
+  const mainContent = document.querySelector(".fade-section");
 
-  // După 3 secunde, loaderul dispare
+  // După 3 secunde dispare loaderul
   setTimeout(() => {
     loader.classList.add("hidden");
 
-    // După un mic delay, activează blur-in pentru video
+    // După un mic delay apare videoclipul + conținutul fade-in
     setTimeout(() => {
       videoBg.classList.add("active");
+      mainContent.classList.add("active");
     }, 800);
   }, 3000);
 });
