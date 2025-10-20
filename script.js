73185eed8120e70360f473d288af6a9e93641981
andz7z <al277@student.ugal.ro>
2025-10-20 17:05:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,16 @@
 window.addEventListener("load", () => {
   const loaderScreen = document.querySelector(".loader-screen");
   const bgVideo = document.getElementById("bg-video");
-  const homeSection = document.getElementById("home");
 
-  // Show video after loader
   setTimeout(() => {
+    // Blur out loader
     loaderScreen.style.filter = "blur(10px)";
     loaderScreen.style.opacity = "0";
+
+    // Blur in video
     bgVideo.style.filter = "blur(0px)";
     bgVideo.style.opacity = "1";
 
     setTimeout(() => loaderScreen.remove(), 1000);
   }, 3000);
-
-  // Hide video when user scrolls past home
-  window.addEventListener("scroll", () => {
-    const scrollY = window.scrollY;
-    const homeHeight = homeSection.offsetHeight;
-
-    if (scrollY > homeHeight - 200) {
-      bgVideo.style.opacity = "0";
-    } else {
-      bgVideo.style.opacity = "1";
-    }
-  });
 });
