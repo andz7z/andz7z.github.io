98452dc5e9f3f2b9804d4d48057ade5c0635e488
andz7z <al277@student.ugal.ro>
2025-10-21 20:42:15 +0300

    Create home.js

---
+++ js/home.js
@@ -0,0 +1,8 @@
+/* --- JS/HOME.JS --- */
+// Logica specifică pentru secțiunea Home.
+// De exemplu: animații complexe la hover pe titlu,
+// sau schimbarea textului la intervale regulate.
+
+document.addEventListener('DOMContentLoaded', () => {
+    // console.log("Logica Home încărcată.");
+});
