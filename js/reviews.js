69a54097d332d92d775c37c80dbcb18e802446df
andz7z <al277@student.ugal.ro>
2025-10-22 18:43:13 +0300

    Update reviews.js

--- js/reviews.js
+++ js/reviews.js
@@ -1,1 +1,5 @@
-// Logica JavaScript specifică pentru secțiunea #reviews
+// reviews.js
+function initReviews() {
+    // Aici s-ar putea adăuga un carusel pentru recenzii dacă este necesar
+    // Momentan, afișăm toate recenziile simultan
+}
