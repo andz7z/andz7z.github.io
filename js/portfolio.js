5ee86b84a1cf2f23e9fed6cadf1c9a031bacf535
andz7z <al277@student.ugal.ro>
2025-10-22 13:21:42 +0300

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,17 +1,1 @@
 // Logica JavaScript specifică pentru secțiunea #portfolioo
-// portfolio.js
-function initPortfolio() {
-    // Modal pentru detalii proiect
-    initProjectModal();
-}
-
-function initProjectModal() {
-    const viewButtons = document.querySelectorAll('.view-project');
-    
-    viewButtons.forEach(button => {
-        button.addEventListener('click', function() {
-            // Într-o implementare reală, aici s-ar deschide un modal cu detalii despre proiect
-            alert('Detalii proiect vor fi afișate aici într-o implementare completă.');
-        });
-    });
-}
