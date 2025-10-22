0681adaa8f1b2d923c1602040a6af259bbef8b02
andz7z <al277@student.ugal.ro>
2025-10-22 13:11:58 +0300

    Update services.js

--- js/services.js
+++ js/services.js
@@ -1,1 +1,30 @@
 // Logica JavaScript specifică pentru secțiunea #services
+// services.js
+function initServices() {
+    // Efecte 3D pentru carduri
+    init3DCards();
+}
+
+function init3DCards() {
+    const serviceCards = document.querySelectorAll('.service-card');
+    
+    serviceCards.forEach(card => {
+        card.addEventListener('mousemove', function(e) {
+            const cardRect = this.getBoundingClientRect();
+            const cardCenterX = cardRect.left + cardRect.width / 2;
+            const cardCenterY = cardRect.top + cardRect.height / 2;
+            
+            const mouseX = e.clientX - cardCenterX;
+            const mouseY = e.clientY - cardCenterY;
+            
+            const rotateY = (mouseX / cardRect.width) * 10;
+            const rotateX = -(mouseY / cardRect.height) * 10;
+            
+            this.style.transform = `perspective(1000px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) translateZ(10px)`;
+        });
+        
+        card.addEventListener('mouseleave', function() {
+            this.style.transform = 'perspective(1000px) rotateX(0) rotateY(0) translateZ(0)';
+        });
+    });
+}
